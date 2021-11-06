1e5b521f1d50b078aeabbd70409645415d0bffa3
Ashik <alamin.ashik160@gmail.com>
2021-11-06 19:46:56 +0600

    Added IntegerExtension class and Int[] fill extension methods

---
+++ ExtensionMethods/IntegerExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mash.HelperMethods.NET.ExtensionMethods
+{
+    public static class IntegerExtension
+    {
+        public static void Fill(this int[] array, int value)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = value;
+            }
+
+        }
+    }
+}
